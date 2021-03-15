76f0b2ecdb0bd1a24ad62515a1d5878e28cff4f5
javiermolsanz <javier.molinasanz@vonage.com>
2021-03-15 14:06:06 +0000

    refactoring with funcitonal mixin

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
-const button = document.getElementById("btn");
+const compareMeBtn = document.getElementById("btn");
 const moreDataBtn = document.getElementById("generateFacts");
-// Create Dino Constructor
-function Dino(species, weight, height, diet, where, when, fact) {
-  (this.species = species),
-    (this.weight = weight),
-    (this.height = height),
-    (this.diet = diet),
-    (this.where = where),
-    (this.when = when),
-    (this.fact = [fact]);
+
+//create Dino functional mixin with the three comparissons
+function Dino(object) {
+  return Object.assign({}, object, {
+    weightFact: function(humanWeight) {
+      if (this.weight > humanWeight) {
+        this.fact.push(
+          `This dino weighted ${this.weight - humanWeight} pounds more than you`
+        );
+      } else if (this.weight === humanWeight) {
+        this.fact.push(`I can't believe that you weight the same as this dino`);
+      } else {
+        this.fact.push(
+          `You weighted ${humanWeight - this.weight} pounds more than this dino`
+        );
+      }
+    },
+    dietFact: function(humanDiet) {
+      const fact =
+        this.diet === humanDiet
+          ? `This dino had the same diet as you`
+          : `Unlike you, this dino had a ${this.diet} diet`;
+      this.fact.push(fact);
+    },
+    heightFact: function(humanHeight) {
+      if (this.height > humanHeight) {
+        this.fact.push(
+          `This dino was ${this.height - humanHeight} feet higher than you`
+        );
+      } else if (this.height === humanHeight) {
+        this.fact.push(`I can't believe that you are as high this dino`);
+      } else {
+        this.fact.push(
+          `You are ${humanHeight - this.height} feet higher than the dino`
+        );
+      }
+    }
+  });
 }
-
-//comparison 1
-Dino.prototype.weightFact = function(humanWeight) {
-  if (this.weight > humanWeight) {
-    this.fact.push(
-      `This dino weighted ${this.weight - humanWeight} pounds more than you`
-    );
-  } else if (this.weight === humanWeight) {
-    this.fact.push(`I can't believe that you weight the same as this dino`);
-  } else {
-    this.fact.push(
-      `You weighted ${humanWeight - this.weight} pounds more than this dino`
-    );
-  }
-};
-
-//comparisson 2
-
-Dino.prototype.dietFact = function(humanDiet) {
-  const fact =
-    this.diet === humanDiet
-      ? `This dino had the same diet as you`
-      : `Unlike you, this dino had a ${this.diet} diet`;
-  this.fact.push(fact);
-};
-
-//
-Dino.prototype.heightFact = function(humanHeight) {
-  if (this.height > humanHeight) {
-    this.fact.push(
-      `This dino was ${this.height - humanHeight} feet higher than you`
-    );
-  } else if (this.height === humanHeight) {
-    this.fact.push(`I can't believe that you are as high this dino`);
-  } else {
-    this.fact.push(
-      `You are ${humanHeight - this.height} feet higher than the dino`
-    );
-  }
-};
 
 // Create Human Object
 function Human(name, weight, inches, feet, diet) {
@@ -67,15 +56,24 @@ const getData = async () => {
 
   dinoData.Dinos.map(element => {
     dinoArray.push(
-      new Dino(
-        element.species,
-        element.weight,
-        element.height,
-        element.diet,
-        element.where,
-        element.when,
-        element.fact
-      )
+      Dino({
+        species: element.species,
+        weight: element.weight,
+        height: element.height,
+        diet: element.diet,
+        where: element.where,
+        when: element.when,
+        fact: [element.fact]
+      })
+      //   new Dino(
+      //     element.species,
+      //     element.weight,
+      //     element.height,
+      //     element.diet,
+      //     element.where,
+      //     element.when,
+      //     element.fact
+      //   )
     );
   });
 
@@ -92,7 +90,7 @@ const getData = async () => {
   return { dinoArray, human };
 };
 
-//const populateFacts =
+//function that adds all possible random facts to all dinos but pigeon
 const populateFacts = dinos => {
   dinos.forEach(dino => {
     if (dino.species != "Pigeon") {
@@ -101,19 +99,16 @@ const populateFacts = dinos => {
       dino.heightFact(human.height);
     }
   });
-  //console.log(dinoArray);
   return dinoArray;
 };
-
+//function that appends the tiles to the dom.
 const addTiles = (dinos, humanName) => {
   const humanTile = {
     species: humanName,
     fact: "boring human"
   };
   insertAt(dinos, 4, humanTile);
-  console.log(dinos);
   const grid = document.getElementById("grid");
-  console.log(humanName);
   for (let i = 0; i < dinos.length; i++) {
     if (i === 4) {
       const html = `<div class="grid-item">
@@ -123,7 +118,6 @@ const addTiles = (dinos, humanName) => {
       grid.insertAdjacentHTML("afterbegin", html);
     } else {
       const dinoToShow = dinos[i];
-      console.log(dinoToShow);
       const factToShow =
         dinoToShow.fact[Math.floor(Math.random() * dinoToShow.fact.length)];
       const html = `<div class="grid-item">
@@ -136,39 +130,28 @@ const addTiles = (dinos, humanName) => {
   }
 };
 
-button.addEventListener("click", function(e) {
+//EVENT LISTENERS
+
+compareMeBtn.addEventListener("click", function(e) {
   e.preventDefault();
-  // Remove form from screen
   hideForm();
   getDataandDisplay();
-  //this gets me human data
-  //   getData().then(data => {
-  //     ({ dinoArray, human } = data);
-  //     const humanName = human.name;
-  //     const dinoWithFacts = populateFacts(dinoArray);
-  //     addTiles(dinoWithFacts, humanName);
-  //     moreDataBtn.style.display = "block";
-  //   });
 });
 
+//event listener that generates new data
 moreDataBtn.addEventListener("click", function(e) {
   e.preventDefault();
   removeTiles(grid);
-  //   getData().then(data => {
-  //     ({ dinoArray, human } = data);
-  //     const humanName = human.name;
-  //     const dinoWithFacts = populateFacts(dinoArray);
-  //     addTiles(dinoWithFacts, humanName);
-  //     moreDataBtn.style.display = "block";
-  //   });
   getDataandDisplay();
 });
 
+//function to hide the form from the screen
 const hideForm = () => {
   const form = document.getElementById("dino-compare");
   form.style.display = "none";
 };
 
+//function that get the data (dino+human) and appends it to the dom. It gets called twice
 const getDataandDisplay = () => {
   getData().then(data => {
     ({ dinoArray, human } = data);
@@ -179,10 +162,12 @@ const getDataandDisplay = () => {
   });
 };
 
+//utility to insert the human object in the middle of the data array
 const insertAt = (array, index, ...elementsArray) => {
   array.splice(index, 0, ...elementsArray);
 };
 
+//utility to remove the tiles before adding new data to the dom
 const removeTiles = parent => {
   while (parent.firstChild) {
     parent.removeChild(parent.lastChild);
